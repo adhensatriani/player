e2ea5f39bb8022e88d77c7aa8c7d154edc4e8090
adhensatriani <54734116+adhensatriani@users.noreply.github.com>
2019-10-27 09:48:48 +0700

    Update video-663.js

--- video-663.js
+++ video-663.js
@@ -5113,7 +5113,7 @@ MediaError.errorTypes = ['MEDIA_ERR_CUSTOM', 'MEDIA_ERR_ABORTED', 'MEDIA_ERR_NET
 MediaError.defaultMessages = {
   1: 'You aborted the media playback',
   2: 'A network error caused the media download to fail part-way.',
-  3: 'The media playback was aborted due to a corruption problem or because the media used features your browser did not support.',
+  3: 'The video will be available, 5 minutes before kick off - IntanTV.com',
   4: 'The media could not be loaded, either because the server or network failed or because the format is not supported.',
   5: 'The media is encrypted and we do not have the keys to decrypt it.'
 };
